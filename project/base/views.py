9d1c8de6df92d3e3bbbfc0856746cfc99c7492f6
BMatthewArzola <benedicy12@gmail.com>
2023-12-29 14:17:02 +0800

    29/12

--- project/base/views.py
+++ project/base/views.py
@@ -771,14 +771,11 @@ def inb_applicant_list(request, status):
 
         applicants_list = model_class.objects.all()
         paginator = Paginator(applicants_list, 10)
-        page_number = request.GET.get('page')
+        page_number = request.GET.get("page")
         applicants = paginator.get_page(page_number)
 
         form = GradeUploadForm(request.POST, request.FILES)
-        return render(request, template, {
-            "applicants": applicants, 
-            "form": form
-            })
+        return render(request, template, {"applicants": applicants, "form": form})
     else:
         messages.error(request, "You don't have permission.")
         return redirect("home")
